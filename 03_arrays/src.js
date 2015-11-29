923ee26b65ff3f361e18787e1803fe8d3b4aa078
JessicaHerford <jrherford12@gmail.com>
2015-11-28 21:15:01 -0700

    More Answers

--- 03_arrays/src.js
+++ 03_arrays/src.js
@@ -22,25 +22,52 @@ module.exports = {
     if(array.length ===0){
     return [];
   }
-    else if(n > array.length){
-    return array(0, array.length-n);
-  }
+    else if(n < array.length){
+      array = array.slice(0,n);
+      return array;
+}
     else{
     return array;
   }
-}
+},
 
+  last_n: function(array, n){
+    if(array.length === 0){
+      return [];
+    }
+    else if(n < array.length){
+      array = array.slice(n-1, array.length);
+      return array;
+    }
+    else {
+      return array;
+    }
+  },
 
-// var origArray = [3, 5, 7, 9];
-// var newArray = origArray. slice(0, -1);
-// document.write(origArray);
-// document.write("<br/>");
-// newArray = origArray. slice(-2);
-// document.write(newArray);
-//
-// // Output:
-// // 3,5,7,9
-// // 7,9
+  drop: function(array,n){
+    if(array.length > n){
+      array = array.slice(n, array.length);
+      return array;
+    }
+    else{
+      return [];
+    }
+
+  },
+
+  union: function(array1, array2){
+      if(array1.length ===0 && array2.length ===0){
+        return [];
+      }
+      else{
+        array = array1.concat(array2);
+        return array;
+      }
+  },
+
+  intersection: function(array1, array2){
+
+  }
 
 
 
