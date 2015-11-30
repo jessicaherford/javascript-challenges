73a0c79acccf35f39f51d1c1b6fec63f551f96cc
JessicaHerford <jrherford12@gmail.com>
2015-11-29 18:01:07 -0700

    06_join_for_loop answers

--- 06_join_for_loop/src.js
+++ 06_join_for_loop/src.js
@@ -5,7 +5,44 @@
 module.exports = {
 
   joinWithFor: function(list) {
-    // your code here
-  }
+  return list.join('');
+},
+
+  joinWithForAndIndex: function(array){
+    var newArray = [];
+    for(var i = 0;i<array.length; i++){
+      newArray.push(array[i] + (i));
+    }
+    return newArray.join('');
+  },
+
+  joinWithoutOddCharacters: function(array){
+    var newArray = [];
+    for(var i = 0; i<array.length; i++){
+      if(i%2===0){
+        newArray.push(array[i])
+      }
+    }
+    return newArray.join('');
+  },
+
+  joinWithForAndToken: function(list, token){
+    return list.join(token);
+  },
+
+  joinWithForAndAlternatingTokens: function(array, token1, token2){
+    list = '';
+    for(i=0;i<array.length; i++){
+      if(i%2===0){
+        list+= array[i];
+        list+= token1;
+      }
+      else{
+        list += array[i];
+        list += token2;
+      }
+    }
+    return list.substring(0,list.length-1);
+    }
 
 }
